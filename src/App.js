e0afb949bdbd3c0602017bee4a64c5df530bd096
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-09-25 09:41:59 +0300

    Kritini 4 life

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+// Define the data for each day
+var scheduleData = {
+  Pirmdiena: [
+    ['Sistemu programmesana', 'Kritins', '203'],
+    ['Dabaszinibas', 'Strade lkm', '210']
+  ],
+  Otrdiena: [
+    ['Sistemu programmesana', 'Kritins', '203'],
+    ['Dabaszinibas', 'Strade lkm', '210']
+  ],
+  Tresdiena: [
+    ['Sistemu programmesana', 'Kritins', '203'],
+    ['Patstavigais darbs', 'Patstavigi', '0']
+  ],
+  Ceturtdiena: [
+    ['Sistemu programmesana', 'Kritins', '203'],
+    ['Patstavigais darbs', 'Patstavigi', '0']
+  ],
+  Piektdiena: [
+    ['Sistemu programmesana', 'Kritins', '203'],
+    ['Patstavigais darbs', 'Patstavigi', '0']
+  ]
+};
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Function to create a table row
+function createTableRow(table, data) {
+  var row = table.insertRow();
+  for (var i = 0; i < data.length; i++) {
+    var cell = row.insertCell(i);
+    cell.textContent = data[i];
+  }
 }
 
-export default App;
+// Function to create a table for a specific day
+function createTable(day, data) {
+  var table = document.createElement('table');
+  table.style.width = '100%';
+
+  var headerRow = table.createTHead().insertRow();
+  var headers = ['Stundas', 'Skolotaja', 'Kabinets'];
+
+  for (var i = 0; i < headers.length; i++) {
+    var th = document.createElement('th');
+    th.textContent = headers[i];
+    headerRow.appendChild(th);
+  }
+
+  for (var i = 0; i < data.length; i++) {
+    createTableRow(table, data[i]);
+  }
+
+  var dayHeader = document.createElement('h4');
+  dayHeader.textContent = day;
+  document.body.appendChild(dayHeader);
+  document.body.appendChild(table);
+}
+
+// Create tables for each day using the data
+for (var day in scheduleData) {
+  createTable(day, scheduleData[day]);
+}
