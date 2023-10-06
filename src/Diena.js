9bd5d9f7d74b0d48109febc984e3bf5cd3c42e7f
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-06 11:31:16 +0300

    lohsesmues

--- src/Diena.js
+++ src/Diena.js
@@ -1,15 +1,16 @@
 import Stunda from './Stunda';
 function Diena(props){
-  const StundasJSX = props.Stundas.map(() =>{};
+  const stundasJSX = props.stundas.map((stunda) =>{
+    return <Stunda nosaukums={stunda}/>
+  });
+
   return(
     <>
     <div class ="emils">
         <h2>{props.diena}</h2>
         <ol type="1">
-    <Stunda nosaukums={props.Stundas[0]}/>
-    <Stunda nosaukums={props.Stundas[1]}/>
-    <Stunda nosaukums={props.Stundas[2]}/>
-    <Stunda nosaukums={props.Stundas[3]}/>
+          {stundasJSX}
+ 
     </ol>
     </div>
     </>
